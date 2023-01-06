05d82f69ca6c9f681816c74c9f2f17fbc3ecc308
Szczepan <szczep.kowalik@gmail.com>
2023-01-06 16:18:05 +0100

    Added searchComponent to routing and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCocktailRecipeComponent } from './cocktail-recipes/add-cocktail-recipe/add-cocktail-recipe.component';
 import { CocktailRecipesComponent } from './cocktail-recipes/cocktail-recipes.component';
+import { SearchCocktailRecipesComponent } from './cocktail-recipes/search-cocktail-recipes/search-cocktail-recipes.component';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'cocktailRecipes', component: CocktailRecipesComponent },
   { path: 'cocktailRecipes/add', component: AddCocktailRecipeComponent },
+  { path: 'cocktailRecipes/search', component: SearchCocktailRecipesComponent },
   { path: '**', redirectTo: '' }
 ];
 
